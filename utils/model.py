d699c578456fd959e79eff438da63d7e5f062a7c
TheZiElU <u64832@student.wat.edu.pl>
2025-04-28 10:14:31 +0200

    chore: cleaing repository

--- utils/model.py
+++
@@ -1,7 +0,0 @@
-users: list = [
-    {"name": "Igor", "location": "Koszalin", "posts": 911, 'picture': 'https://www.wig.wat.edu.pl/images/aktualnosci/2025/sklad_kpn_700.jpg'},
-    {"name": "Wiktoria", "location": "Chełm", "posts": 6, 'picture': 'https://i1.sndcdn.com/artworks-ACVdFsOSQyupQv3b-UkcUtA-t500x500.png'},
-    {"name": "Sabina", "location": "Opole", "posts": 110, 'picture': 'https://i1.sndcdn.com/artworks-ACVdFsOSQyupQv3b-UkcUtA-t500x500.png'},
-    {"name": "Weronika", "location": "Tomaszów Mazowiecki", "posts": 350, 'picture': 'https://i1.sndcdn.com/artworks-ACVdFsOSQyupQv3b-UkcUtA-t500x500.png'},
-    {"name": "Oliwka", "location": "Warszawa", "posts": 420, 'picture': 'https://i1.sndcdn.com/artworks-ACVdFsOSQyupQv3b-UkcUtA-t500x500.png'},
-]
